e0f7fcf2c6b41449331ff860340c5257cf7ecf44
Rico-Developpeur <eric.vieira@sncf.fr>
2022-05-13 18:04:16 +0200

    ajout du favicon

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const img = document.createElement("img");
-img.src = "courrier.gif";
+img.src = "images/courrier.gif";
 img.style.width = "200px";
-// const d = document.
+
 const affiche = document.querySelector("#contactForm");
 
 
 affiche.addEventListener("click", function (event) {
     event.preventDefault();
-    // J'efface le contenu du formulaire
-    affiche.innerHTML = '';
-    // je remplace par le GIF
+    // efface le contenu du form, remplace par :
+    affiche.innerHTML = '📜 Message envoyé 📜';
     affiche.appendChild(img);
 })
